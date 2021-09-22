6a5dc7c8a30f18187376d965fb0e35e04bc8db5a
Rofita Siti Musdalifah <rofitasitimusdalifah@gmail.com>
2021-09-22 16:46:08 +0700

    add plugin

--- src/test/java/org/example/acceptancetests/AcceptanceTestSuite.java
+++ src/test/java/org/example/acceptancetests/AcceptanceTestSuite.java
@@ -5,5 +5,5 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features/", glue="org.example", tags = "@Pokemon", stepNotifications = true)
+@CucumberOptions(features="src/test/resources/features/", plugin = {"json:target/destination/cucumber.json"} ,glue="org.example", tags = "@Pokemon", stepNotifications = true)
 public class AcceptanceTestSuite {}
